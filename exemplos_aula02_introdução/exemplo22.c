21884f4acadd56c56eec19592ce2a203ee7cd41e
Pedro Nicolas Costa <pedroniickolas@gmail.com>
2024-04-05 12:09:01 -0300

    Add files via upload

--- exemplos_aula02_introdução/exemplo22.c
+++ exemplos_aula02_introdução/exemplo22.c
@@ -1,3 +1,32 @@
 //Exemplo 22
-//
+//Programa que calcula a média entre duas notas reais, no fim imprime a mensagem Aprovado, para o caso onde a média seja maior ou igual a 6, caso contrário imprime reprovado.
 //Autor: Pedro Nicolas Costa
+
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	
+	float nota1, nota2, media;
+	
+	printf("Digite a NOTA 1 do aluno: \n");
+	scanf("%f", &nota1);
+	
+	printf("Digite a NOTA 2 do aluno: \n");
+	scanf("%f", &nota2);
+	
+	media = (nota1 + nota2) / 2;
+	
+	printf("A média do aluno é = %.2f", media);
+	
+	if(media>= 6){
+		printf("\nAPROVADO!");
+	}
+	
+	else{
+		printf("\nREPROVADO!");
+	}
+	
+	return 0;
+}
--- exemplos_aula02_introdução/exemplo23.c
+++ exemplos_aula02_introdução/exemplo23.c
@@ -1,3 +1,24 @@
 //Exemplo 23
-//
+//Exemplo que imprime um valor armazenado em uma variável inteira, além disso, temos dois elementos novos, o clrscr() e o getch(), que pertencem a biblioteca conio.h
 //Autor: Pedro Nicolas Costa
+
+#include <stdio.h>
+#include <locale.h>
+#include <conio.h> //necessario para as funcoes clrscr e getch
+
+void main(){
+	setlocale(LC_ALL, "Portuguese");
+	
+	int Contador;
+	int NroDeFilhos;
+	
+	Contador = 10;
+	printf("Valor da variável: %d \n", Contador);
+	
+	NroDeFilhos = 3;
+	printf("Maria tem %d filhos", NroDeFilhos);
+	
+	getch(); //espera que o usuario pressione uma tecla
+	
+	//clrscr(); //limpa a tela
+}
